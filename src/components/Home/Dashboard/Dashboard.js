1132338a84eb6a88cc77882abac32a6d861d0190
Your Name <abhishakya1998@gmail.com>
2023-10-17 22:52:08 +0530

    search component

--- src/components/Home/Dashboard/Dashboard.js
+++ src/components/Home/Dashboard/Dashboard.js
@@ -6,9 +6,15 @@ import ProductCard from './ProductCard/ProductCard'
 export default function Dashboard() {
     return (
         <div className='dashboard-component'>
+            <div className='search-section'>
+                <input type='text' className='search-text-field' placeholder='Type to search for products'/>
+            </div>
+            <div className='content-section'>
             {productsData.map((product, index) => {
                 return <ProductCard key={index} product={product} />
             })}
+
+            </div>
         </div>
     )
 }
